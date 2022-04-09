d459dc8959e2d5e93218f3cb14970d284455f084
LAVIESTES <73939435+code-fs@users.noreply.github.com>
2022-04-09 15:58:28 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,25 @@
 import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(commant_prefix=commands.when_mentioned_or("p!"), intents=intents)
+
+@bot.event
+async def on_ready():
+  print("Ready To Help!")
+  
+@bot.event
+async def on_member_join(member):
+  channel = client.get_channel(901099381665718303)
+  embed = discord.Embed(
+    title = "Welcome to the server",
+    description = f"How are you doing? Before hoping into the fun, make sure to read the rules in <#962096867783626812> and join the team by opening a ticket in <#962096867783626812> then follow along.",
+    color = 0xFF6500
+  )
+  embed.set_author(name=mention)
+  await channel.send('hello')
+
+  client.run(os.environ['TOKEN'])
